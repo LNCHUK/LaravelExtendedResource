8b539e1d9dd8aaa1f66bb4de41c2631907fc5aa7
Tom Lynch <tom@lnch.co.uk>
2020-05-05 21:00:44 +0100

    Initial commit

---
+++ src/LaravelExtendedResourceServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LNCHUK\LaravelExtendedResource;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelExtendedResourceServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
